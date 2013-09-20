af05cee3b99efca4d79315307ad227d16a3c1a85
gmorell <gmp@h4xmb.org>
2013-09-19 21:09:42 -0400

    made the data work

--- acct/admin.py
+++ acct/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
 from acct.models import Profile
+from acct.models import Orgsync_User,Orgsync_Org,Orgsync_OrgCat
 from projection.models import Projectionist
 
 # doodeedoo
@@ -18,7 +19,15 @@ class ProjectionistInline(admin.StackedInline):
 # Define a new User admin
 class UserAdmin(UserAdmin):
     inlines = (AcctInline, ProjectionistInline)
+    
+class OOAdmin(admin.ModelAdmin):
+    search_fields = ('name','orgsync_id')
 
 # Re-register UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+
+admin.site.register(Orgsync_User)
+admin.site.register(Orgsync_Org,OOAdmin)
+admin.site.register(Orgsync_OrgCat)
+    
--- acct/models.py
+++ acct/models.py
@@ -19,3 +19,40 @@ def create_user_profile(sender, instance, created, **kwargs):
         Profile.objects.create(user=instance)
 
 post_save.connect(create_user_profile, sender=User)
+
+class Orgsync_OrgCat(models.Model):
+    name = models.CharField(max_length=64)
+    orgsync_id = models.IntegerField()
+    def __unicode__(self):
+        return "<OrgSyncOrgCat (%s)>" % self.name
+class Orgsync_Org(models.Model):
+    orgsync_id = models.IntegerField()
+    name = models.CharField(max_length=128)
+    category = models.ForeignKey(Orgsync_OrgCat,null=True,blank=True)
+    keywords = models.TextField(null=True,blank=True)
+    president_email = models.EmailField(null=True,blank=True)
+    org_email = models.EmailField(null=True,blank=True)
+    def __unicode__(self):
+        return "<OrgSyncOrg (%s)>" % self.name
+class Orgsync_User(models.Model):
+    orgsync_id  = models.IntegerField()
+    title = models.CharField(max_length=256,null=True,blank=True)
+    account_id = models.IntegerField()
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    email_address = models.EmailField()
+    memberships = models.ManyToManyField(Orgsync_Org,null=True,blank=True)
+    last_login = models.DateField(null=True,blank=True)
+    about_me = models.TextField(null=True,blank=True)
+    portfolio=models.CharField(max_length=256,null=True,blank=True)
+    
+    def __unicode__(self):
+        return "<OrgSyncUser (%s,%s)>" % (self.first_name,self.last_name)
+    
+#example {"id":918421,"title":null,"account_id":575310,"first_name":"Aakriti","last_name":"Bhakhri","picture_url_40":"https://d1nrm4vx8nf098.cloudfront.net/eupwtt1qw06izh_40.jpg","picture_url_60":"https://d1nrm4vx8nf098.cloudfront.net/eupwtt1qw06izh_64.jpg","memberships":[{"id":152541,"name":"Members"}],"email_address":"aakriti@wpi.edu","last_login":"April 24, 2012","portfolio":"http://my.orgsync.com/aakritibhakhri"},
+#url https://orgsync.com/38382/accounts?per_page=100&num_pages=3&order=first_name+ASC
+#paginatd https://orgsync.com/38382/accounts?per_page=100&num_pages=3&order=first_name+ASC&page=46
+#profile https://orgsync.com/profile/display_profile?id=636887
+#profile2 https://orgsync.com/profile/display_profile?id=575310
+#b.open("https://orgsync.com/38382/groups")
+#b.open("https://orgsync.com/38382/accounts?per_page=100&num_pages=3&order=first_name+ASC")
