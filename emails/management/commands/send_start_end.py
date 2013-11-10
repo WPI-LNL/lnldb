bf408c5d21532dd3b01852a6674759759d6202ea
gmorell <gmp@h4xmb.org>
2013-11-10 17:03:40 -0500

    moar

---
+++ emails/management/commands/send_start_end.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand, CommandError
+from emails.generators import generate_event_start_end_emails
+
+class Command(BaseCommand):
+    help = "Sends Emails for events starting RIGHT NOW"
+    
+    def handle(self,*args,**options):
+        generate_event_start_end_emails()
