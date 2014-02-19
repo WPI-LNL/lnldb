0a696c86759dd70474f86a17001dbd6d3e5caaf3
gmorell <gmp@h4xmb.org>
2014-02-19 12:48:19 -0500

    important fix

--- events/middleware.py
+++ events/middleware.py
@@ -3,9 +3,10 @@ from django.contrib import messages
 
 class ContactReminderMiddleware(object):
     def process_view(self, request, view_func, view_args, view_kwargs):
-        if not (request.user.first_name and request.user.last_name):
-            #messages.add_message(request, messages.WARNING, type(view_func.func_name))
-            if "acctupdate" not in view_func.func_name.lower():# == "acctupdate" or "i18n_javascript":
-                messages.add_message(request, messages.ERROR, 'Please visit <a href="/my/acct/">My Account</a> and update your information')
+        if request.user.is_authenticated():
+            if not (request.user.first_name and request.user.last_name):
+                #messages.add_message(request, messages.WARNING, type(view_func.func_name))
+                if "acctupdate" not in view_func.func_name.lower():# == "acctupdate" or "i18n_javascript":
+                    messages.add_message(request, messages.ERROR, 'Please visit <a href="/my/acct/">My Account</a> and update your information')
             
         return None
