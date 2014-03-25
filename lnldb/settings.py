303ab12d7875b52a3534cbe58ee5471080aed9bc
gmorell <gmp@h4xmb.org>
2014-03-25 08:48:56 -0400

    changes for django1.6

--- lnldb/settings.py
+++ lnldb/settings.py
@@ -126,7 +126,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'django.contrib.markup',
+    'markdown_deux',
     'django_cas',
     
     'events',
