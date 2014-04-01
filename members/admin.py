f944e493bfd70c5e4f91fbbf2ac45ce2d29ee5c9
gmorell <gmp@h4xmb.org>
2014-04-01 17:52:29 -0400

    added last_login field to djadmin #251

--- members/admin.py
+++ members/admin.py
@@ -4,3 +4,14 @@ from members.models import StatusChange
 class SCAdmin(admin.ModelAdmin):
     list_display = ["member","group_list","date"]
 admin.site.register(StatusChange,SCAdmin)
+
+
+# to add last login to the django admin site
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+UserAdmin.list_display += ('last_login',)
+UserAdmin.list_filter += ('last_login',)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
