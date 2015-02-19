49ca4616a88dfde52d75bfc97665c3c73fa18ff8
jmerdich <jmerdich@users.noreply.github.com>
2015-02-19 01:13:27 -0500

    Disable DEBUG on prod.

--- lnldb/settings.py
+++ lnldb/settings.py
@@ -4,7 +4,7 @@ from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
 import os
 here = lambda *x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
