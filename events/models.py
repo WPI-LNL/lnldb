421469fe4ccecf652b7030b61784476174c0b0dd
Jake Merdich <jake@merdich.com>
2015-04-27 15:51:03 -0400

    Fix a backend ui issue w/ rqd fields.

--- events/models.py
+++ events/models.py
@@ -343,7 +343,7 @@ class Event(models.Model):
     contact = models.ForeignKey(User, null=True, blank=True, verbose_name="Contact")
     org = models.ManyToManyField('Organization', blank=True, verbose_name="Client")
     billing_org = models.ForeignKey('Organization', null=True, blank=True, related_name="billedevents")
-    billing_fund = models.ForeignKey('Fund', null=True, on_delete=models.SET_NULL, related_name="event_accounts")
+    billing_fund = models.ForeignKey('Fund', null=True, blank=True, on_delete=models.SET_NULL, related_name="event_accounts")
     contact_email = models.CharField(max_length=256, null=True, blank=True)  # DEPRECATED
     contact_addr = models.TextField(null=True, blank=True)  # DEPRECATED
     contact_phone = models.CharField(max_length=32, null=True, blank=True)  # DEPRECATED
