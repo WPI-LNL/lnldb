2f996bfb2e4e18a012ef44d8501aa1a3ec09e25a
Jake Merdich <jake@merdich.com>
2015-06-19 22:31:06 -0700

    Fix login.

--- acct/forms.py
+++ acct/forms.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django import forms
 from django.core.urlresolvers import reverse
@@ -5,8 +6,16 @@ from django.core.urlresolvers import reverse
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Field, HTML
 from crispy_forms.bootstrap import FormActions
+from django.utils.translation import ugettext_lazy as _
 
 from acct.models import Profile
+
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        self.helper = FormHelper()
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.helper.add_input(Submit('submit', 'Log In Locally', css_class="btn btn-lg btn-block btn-info"))
 
 
 class UserAcct(forms.ModelForm):
