93aec780662b600f8da17612c462501d490031a5
Jake Merdich <jmerdich@users.noreply.github.com>
2015-08-21 01:45:55 -0700

    Update context_processors.py

--- data/context_processors.py
+++ data/context_processors.py
@@ -4,3 +4,6 @@ from django.conf import settings
 def airplane_mode(request):
     # return the value you want as a dictionnary. you may add multiple values in there.
     return {'AIRPLANE_MODE': settings.AIRPLANE_MODE}
+    
+def analytics(request):
+    return {'GA_ID': settings.GA_ID}
