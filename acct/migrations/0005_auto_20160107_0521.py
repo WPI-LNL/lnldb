a90e36ab4ed032c7e10cc2c4f224574cd83cfcb0
LNL Webmaster <lnl-w@wpi.edu>
2016-01-07 05:46:29 -0500

    Old model removal migrations.

---
+++ acct/migrations/0005_auto_20160107_0521.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('acct', '0004_Perms typos'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='orgsync_org',
+            name='category',
+        ),
+        migrations.RemoveField(
+            model_name='orgsync_user',
+            name='memberships',
+        ),
+        migrations.RemoveField(
+            model_name='profile',
+            name='user',
+        ),
+        migrations.DeleteModel(
+            name='Orgsync_Org',
+        ),
+        migrations.DeleteModel(
+            name='Orgsync_OrgCat',
+        ),
+        migrations.DeleteModel(
+            name='Orgsync_User',
+        ),
+        migrations.DeleteModel(
+            name='Profile',
+        ),
+    ]
---
+++ members/migrations/0002_auto_20160107_0520.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('members', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='statuschange',
+            name='groups',
+        ),
+        migrations.RemoveField(
+            model_name='statuschange',
+            name='member',
+        ),
+        migrations.DeleteModel(
+            name='StatusChange',
+        ),
+    ]
