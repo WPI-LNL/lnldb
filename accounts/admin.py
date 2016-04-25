40ee83b1476df71c6c31da6846e4cd8454392583
Jake Merdich <jake@merdich.com>
2016-04-25 11:35:29 -0400

    Put the new fields in the admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -8,11 +8,11 @@ from .models import User
 class MemberAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'nickname', 'email')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-        ("Other", {"fields": ["addr", "mdc", "wpibox", "phone"]})
+        ("Other", {"fields": ["addr", "mdc", "wpibox", "phone", "student_id", "locked"]})
     )
 
 
