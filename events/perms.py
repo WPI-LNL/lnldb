69222a61788ebbb7e95fc547c0d63d086bdf9bf2
LNL Webmaster <lnl-w@wpi.edu>
2016-09-19 14:15:43 -0400

    super magic fix for nested permission checks

--- events/perms.py
+++ events/perms.py
@@ -1,7 +1,9 @@
 from django.core.exceptions import PermissionDenied
 from permission.logics import PermissionLogic
 from permission.utils.field_lookup import field_lookup
-
+import logging
+from logging import debug
+logging.basicConfig(filename='/home/lnl/perms.log', level=logging.DEBUG)
 
 class AssocUsersCustomPermissionLogic(PermissionLogic):
     field_name = 'authorized_users'
@@ -11,9 +13,11 @@ class AssocUsersCustomPermissionLogic(PermissionLogic):
     def has_perm(self, user_obj, perm, obj=None):
         # User must be logged in
         if not user_obj.is_authenticated():
+            debug("%s not signed in" % user_obj)
             return False
         # See if we can handle that perm
         if perm not in self.perms and perm not in self.denied:
+            debug("permission %s not recognized" % perm)
             return False
         # if there isn't an object, that means we're looking for a module permission.
         # We don't do that.
@@ -21,6 +25,7 @@ class AssocUsersCustomPermissionLogic(PermissionLogic):
             return False
         # If your account has been disabled, too bad.
         if not user_obj.is_active:
+            debug("%s is not an active user" % user_obj)
             return False
         # get all authorized_users in the object
         if not hasattr(self.field_name, '__iter__'):
@@ -28,13 +33,17 @@ class AssocUsersCustomPermissionLogic(PermissionLogic):
         for lookup in self.field_name:
             authorized_users = field_lookup(obj, lookup)
             # break out of a generator expression
-            # if hasattr(authorized_users, 'next'):
-            #     authorized_users = authorized_users.next()
-            if hasattr(authorized_users, 'all'):
-                authorized_users = authorized_users.all()
-            if user_obj == authorized_users or \
-                    (hasattr(authorized_users, '__iter__') and user_obj in authorized_users):
+            if hasattr(authorized_users, 'next'):
+                authorized_users = list(authorized_users)
+            else:
+                authorized_users = [authorized_users]
+            for user in authorized_users:
+                if hasattr(user, 'all'):
+                    authorized_users.extend(user.all())
+                    authorized_users.remove(user)
+            if (user_obj in authorized_users):
                 if perm in self.denied:
+                    debug("%s denied for %s in lookup '%s'" % (perm, user_obj, lookup))
                     raise PermissionDenied()
                 return True
 
