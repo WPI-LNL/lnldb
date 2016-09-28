310be9f7f1e1a2af69c738283ae0f05c9755bf82
LNL Webmaster <lnl-w@wpi.edu>
2016-09-28 18:09:27 -0400

    Copy howtos to the other cc report forms

--- events/forms.py
+++ events/forms.py
@@ -506,6 +506,13 @@ class InternalReportForm(FieldAccessForm):
         self.helper.form_action = ""
         self.helper.layout = Layout(
             DynamicFieldContainer('crew_chief'),
+            HTML('<h4>What you might put in the report::</h4>'
+                 '<ul><li>How was the event set up?</li>'
+                 '<li>Roughly what equipment was used?</li>'
+                 '<li>Were there any last minute changes</li>'
+                 '<li>Did you come across any issues?</li>'
+                 '<li>Would you classify this event as the level it was booked under?</li>'
+                 '<li>What information would be useful for somebody next year?</li></ul>'),
             Field('report', css_class="col-md-10"),
             markdown_at_msgs,
             FormActions(
