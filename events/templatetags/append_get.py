f578b1a402ebbec89fd3ad78174ffc253f5995f3
Jake Merdich <jake@merdich.com>
2016-10-16 19:11:53 -0400

    Make the querystring generator smarter and smaller instead of using copypasted code

--- events/templatetags/append_get.py
+++ events/templatetags/append_get.py
@@ -2,61 +2,33 @@ from django import template
 
 register = template.Library()
 
-"""
-Decorator to facilitate template tag creation
-"""
+@register.simple_tag(takes_context=True)
+def append_to_get(context, replace=True, **kwargs):
+    """
+    Adds/deletes arguments to the current GET value
+     and returns a querystring containing it.
 
+    @argument replace: If true, any existing argument
+      named in kwargs will have their value overridden.
+      If false, kwargs are appended only.
+    @argument kwargs: key-val pairs to add, with a value of 
+      None for deletion. (Use "None" if you don't want that)
+    """
+    updated = context['request'].GET.copy()
 
-def easy_tag(func):
-    """deal with the repetitive parts of parsing template tags"""
-    def inner(parser, token):
-        # divide token into args and kwargs
-        args = []
-        kwargs = {}
-        for arg in token.split_contents():
-            try:
-                name, value = arg.split('=')
-                kwargs[str(name)] = value
-            except ValueError:
-                args.append(arg)
-        try:
-            # try passing parser as a kwarg for tags that support it
-            extrakwargs = kwargs.copy()
-            return func(*args, **extrakwargs)
-        except TypeError:
-            # otherwise just send through the original args and kwargs
-            try:
-                return func(*args, **kwargs)
-            except TypeError as e:
-                raise template.TemplateSyntaxError('Bad arguments for tag "%s"' % args[0])
-    inner.__name__ = func.__name__
-    inner.__doc__ = inner.__doc__
-    return inner
+    if not replace:
+        # duplicates will be appended to the query
+        updated.update(kwargs)
+    else:
+        # duplicates will be replaced
+        for arg, val in kwargs.items():
+            updated[arg] = val
 
-
-class AppendGetNode(template.Node):
-    def __init__(self, in_dict):
-        self.dict_pairs = {}
-        for key in in_dict:
-            self.dict_pairs[key] = template.Variable(in_dict[key])
-
-    def render(self, context):
-        get = context['request'].GET.copy()
-
-        for key in self.dict_pairs:
-            get[key] = self.dict_pairs[key].resolve(context)
-
-        path = context['request'].META['PATH_INFO']
-
-        # print "&".join(["%s=%s" % (key, value) for (key, value) in get.items() if value])
-
-        if len(get):
-            path += "?%s" % "&".join(["%s=%s" % (key, value) for (key, value) in get.items() if value])
-
-        return path
-
-
-@register.tag()
-@easy_tag
-def append_to_get(_tag_name, **mydict):
-    return AppendGetNode(mydict)
+    # if the kwarg is None delete it instead
+    for arg, val in kwargs.items():
+        if val is None:
+            updated.pop(arg)
+    if updated:
+        return "?" + updated.urlencode()
+    else:
+        return ""
