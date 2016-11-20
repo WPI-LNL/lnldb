d33f428d72e91d046f702975fe14372cd97b1a50
Jake Merdich <jake@merdich.com>
2016-11-20 00:11:07 -0500

    Settings.py performance tuning and PEP8

--- lnldb/settings.py
+++ lnldb/settings.py
@@ -20,13 +20,14 @@ def from_root(*x):
 def from_runtime(*x):
     return os.path.join(from_root('runtime'), *x)
 
+
 try:
-   import raven
-   path = from_root('.')
-   GIT_RELEASE = raven.fetch_git_sha(path)
+    import raven
+    path = from_root('.')
+    GIT_RELEASE = raven.fetch_git_sha(path)
 except Exception as e:
-	print(e)
-	GIT_RELEASE = "unknown build"
+    print(e)
+    GIT_RELEASE = "unknown build"
 
 TESTING = sys.argv[1:2] == ['test']
 
@@ -37,14 +38,15 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'lnl.wpi.edu', 'users.wpi.edu', 'userweb.wpi.edu']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'lnl.wpi.edu',
+                 'users.wpi.edu', 'userweb.wpi.edu']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DATABASES = {
-    'default': { # for testing
+    'default': {  # for testing
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': from_runtime('lnldb.db'),  
+        'NAME': from_runtime('lnldb.db'),
     }
     # #  mysql template
     # 'default': {
@@ -53,7 +55,8 @@ DATABASES = {
     #     'USER': 'lnlcgi',
     #     'PASSWORD': '<...>',
     #     'HOST': 'mysql.wpi.edu',
-    #     'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    #     'PORT': '',
+    #     'CONN_MAX_AGE': 30,
     #     'OPTIONS': {
     #         'sql_mode': 'TRADITIONAL',
     #         'charset': 'utf8mb4',
@@ -65,6 +68,8 @@ DATABASES = {
     #     }  # Now we have a mild degree of confidence :-) Oh, MySQL....
     # }
 }
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -80,11 +85,11 @@ SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-USE_I18N = True
+USE_I18N = False
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale.
-USE_L10N = True
+USE_L10N = False
 TIME_FORMAT = "%I:%M %p"
 DATETIME_FORMAT = '%Y-%m-%d %H:%M'
 TIME_INPUT_FORMATS = ['%I:%M %p', '%I:%M:%S.%f %p', '%I:%M %p',
@@ -130,28 +135,33 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '*am$3w(-v2p+i)m-6t8f0d%)%g60cr+tj6$_x1_u-$wx^0$fu%'
 
+_tmpl_loaders = [
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    ]
+if not DEBUG:
+    _tmpl_loaders = [('django.template.loaders.cached.Loader', _tmpl_loaders)]
+
+
 TEMPLATES = [{
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
     'DIRS': [from_root("site_tmpl"), ],
     'OPTIONS': {
-        'loaders': [
-            ('django.template.loaders.cached.Loader', [
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader',
-            ]),
-        ],
+        'loaders': _tmpl_loaders,
         'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.request',
+                'django.core.context_processors.static',
+                'django.core.context_processors.media',
                 'data.context_processors.airplane_mode',
                 'data.context_processors.revision',
                 'data.context_processors.analytics',
@@ -223,11 +233,10 @@ INSTALLED_APPS = (
     'compat',
 )
 if TESTING:
+    # bypass migrations for unit tests. **MUCH** faster
     try:
-        import test_without_migrations # for really speedy testing!
-        INSTALLED_APPS += (
-            'test_without_migrations',
-                )
+        import test_without_migrations # NOQA
+        INSTALLED_APPS += ('test_without_migrations',)
     except:
         pass
 
@@ -298,7 +307,7 @@ AUTHENTICATION_BACKENDS = (
 # Number of hours to show on the admin landing
 LANDING_TIMEDELTA = 72
 
-# Number of days to pass before crew chief reports are no longer able to be written.
+# Number of days to pass before crew chief reports are locked
 CCR_DAY_DELTA = 7
 
 # email stuff
@@ -331,19 +340,25 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 # Don't mess with builtins just for the sake of permissions
 PERMISSION_REPLACE_BUILTIN_IF = False
 
+
 def reverse_noexcept(url, args=None, kwargs=None):
     try:
         url_out = reverse(url, args=args, kwargs=kwargs)
     except NoReverseMatch:
-        url_out = "#" # dont change the url
+        url_out = "#"  # dont change the url
     return url_out
+
 
 # markdown deux configuration
 MARKDOWN_DEUX_STYLES = {
     "default": {
         "link_patterns": [
-            (re.compile("@([A-Za-z][A-Za-z0-9]*)"), lambda m: reverse_noexcept("accounts:by-name:detail", kwargs={'username':m.group(1)})),
-            (re.compile("@([0-9]+)"), lambda m: reverse_noexcept("events-detail", args=[m.group(1)])),
+            (re.compile("@([A-Za-z][A-Za-z0-9]*)"),
+                lambda m: reverse_noexcept("accounts:by-name:detail",
+                                           kwargs={'username': m.group(1)})),
+            (re.compile("@([0-9]+)"),
+                lambda m: reverse_noexcept("events-detail",
+                                           args=[m.group(1)])),
             ],
         "extras": {
             "code-friendly": None,
@@ -357,7 +372,8 @@ MARKDOWN_DEUX_STYLES = {
     },
 }
 # and for the html editor
-EXTENSIONS = ["newlines", "smart-strong", "strikethrough", "smartypants", "tables"]
+EXTENSIONS = ["newlines", "smart-strong", "strikethrough",
+              "smartypants", "tables"]
 
 CACHES = {
     'default': {
