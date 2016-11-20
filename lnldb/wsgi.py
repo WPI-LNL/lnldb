78d307b733a995c4dc2cb40973346a42dc2c0bbc
Jake Merdich <jake@merdich.com>
2016-11-20 01:40:03 -0500

    PEP8 (wsgi)

--- lnldb/wsgi.py
+++ lnldb/wsgi.py
@@ -32,12 +32,12 @@ application = get_wsgi_application()
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-##from django.core.wsgi import get_wsgi_application
-##application = get_wsgi_application()
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
 
-##from lnldb.wsgi import lnldb
-##application  = lnldb(application)
+# from lnldb.wsgi import lnldb
+# application  = lnldb(application)
