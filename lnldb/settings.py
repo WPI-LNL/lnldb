057fbc1efd262778cc5f702d9464bf08bdc89ba4
Jake Merdich <jake@merdich.com>
2017-07-16 19:06:01 -0700

    Keep working out cloud storage

--- lnldb/settings.py
+++ lnldb/settings.py
@@ -137,6 +137,7 @@ if env.str("AWS_ACCESS_KEY_ID", ""):
     AWS_ACCESS_KEY_ID = env.str('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = env.str('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = env.str('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = env.str("AWS_S3_REGION_NAME", None)
     if env.str("AWS_S3_HOST", ""):
         AWS_S3_HOST = env.str("AWS_S3_HOST")
         S3_USE_SIGV4 = True
