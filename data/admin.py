0b9f4f17c85b256252a11019c1830a8d4cfc4420
Jake Merdich <jake@merdich.com>
2017-10-02 17:57:38 -0400

    Make sure to hide the old redirect admin to reduce confusion

--- data/admin.py
+++ data/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.contrib.redirects.models import Redirect as BaseRedirect
 from .models import ResizedRedirect
 
 class RedirectAdmin(admin.ModelAdmin):
     pass
+admin.site.unregister(BaseRedirect)
 admin.site.register(ResizedRedirect, RedirectAdmin)
