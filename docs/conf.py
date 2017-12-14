a54acf7385753e30139394ba01e2970992acaf7c
Jake Merdich <jake@merdich.com>
2017-12-13 23:28:09 -0500

    Make docs work with Py2/Py3

--- docs/conf.py
+++ docs/conf.py
@@ -177,19 +177,26 @@ texinfo_documents = [
 import inspect
 from django.utils.html import strip_tags
 from django.utils.encoding import force_text
+from django.utils import six
 
 def get_class_that_defined_method(meth):
-    if inspect.ismethod(meth):
-        for cls in inspect.getmro(meth.__self__.__class__):
-           if cls.__dict__.get(meth.__name__) is meth:
+    if six.PY2:
+        for cls in inspect.getmro(meth.im_class):
+            if meth.__name__ in cls.__dict__:
                 return cls
-        meth = meth.__func__  # fallback to __qualname__ parsing
-    if inspect.isfunction(meth):
-        cls = getattr(inspect.getmodule(meth),
-                      meth.__qualname__.split('.<locals>', 1)[0].rsplit('.', 1)[0])
-        if isinstance(cls, type):
-            return cls
-    return getattr(meth, '__objclass__', None)  # handle special descriptor objects
+        return None
+    else:
+        if inspect.ismethod(meth):
+            for cls in inspect.getmro(meth.__self__.__class__):
+               if cls.__dict__.get(meth.__name__) is meth:
+                    return cls
+            meth = meth.__func__  # fallback to __qualname__ parsing
+        if inspect.isfunction(meth):
+            cls = getattr(inspect.getmodule(meth),
+                          meth.__qualname__.split('.<locals>', 1)[0].rsplit('.', 1)[0])
+            if isinstance(cls, type):
+                return cls
+        return getattr(meth, '__objclass__', None)  # handle special descriptor objects
 
 def default_to_parent_docstr(app, what, name, obj, options, lines):
     if inspect.isfunction(obj) and obj.__doc__ is None and len(lines) == 0:
