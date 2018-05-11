cde00b9d0cedce3791b5ce5e96a3f01fd8a8ecb3
Ryan LaPointe <ryan@ryanlapointe.org>
2018-05-11 08:56:32 -0400

    Fix calculation of whether to send email when an event is edited

--- events/views/mkedrm.py
+++ events/views/mkedrm.py
@@ -29,6 +29,14 @@ def eventnew(request, id=None):
             raise PermissionDenied
 
     if request.method == 'POST':
+        if instance:
+            # calculate whether an email should be sent based on the event information *before* saving the form.
+            should_send_email = not instance.test_event
+            if should_send_email:
+                bcc=[settings.EMAIL_TARGET_VP]
+                if instance.projection:
+                    bcc.append(settings.EMAIL_TARGET_HP)
+
         form = InternalEventForm(
             data=request.POST,
             request_user=request.user,
@@ -37,39 +45,35 @@ def eventnew(request, id=None):
 
         if form.is_valid():
             if instance:
-                # calculate whether an email should be sent based on the event information *before* saving the form.
-                should_send_email = not instance.test_event
+                res = form.save()
                 if should_send_email:
-                    bcc=[settings.EMAIL_TARGET_VP]
-                    if instance.projection:
-                        bcc.append(settings.EMAIL_TARGET_HP)
-                    for ccinstance in instance.ccinstances.all():
+                    # BCC the crew chiefs
+                    for ccinstance in res.ccinstances.all():
                         if ccinstance.crew_chief.email:
                             bcc.append(ccinstance.crew_chief.email)
-                    if instance.reviewed:
+                    if res.reviewed:
                         subject = "Reviewed Event Edited"
                         email_body = "The following event was edited by %s after the event was reviewed for billing." % request.user.get_full_name()
                         bcc.append(settings.EMAIL_TARGET_T)
-                    elif instance.approved:
+                    elif res.approved:
                         subject = "Approved Event Edited"
                         email_body = "The following event was edited by %s after the event was approved." % request.user.get_full_name()
                     else:
                         subject = "Event Edited"
                         email_body = "The following event was edited by %s." % request.user.get_full_name()
+                    # Add list of changed fields to the email
                     if len(form.changed_data) > 0:
                         email_body += "\nFields changed: "
                         for field_name in form.changed_data:
                             email_body += field_name + ", "
                         email_body = email_body[:-2]
-                res = form.save()
-                if should_send_email:
                     # add HP to the email if projection was just added to the event
                     if res.projection and not settings.EMAIL_TARGET_HP in bcc:
                         bcc.append(settings.EMAIL_TARGET_HP)
                     to_emails=[]
                     if request.user.email:
                         to_emails.append(request.user.email)
-                    email = EventEmailGenerator(event=res, subject=subject, to_emails=to_emails, body=email_body, bcc=bcc)
+                    email = EventEmailGenerator(event=res, subject=subject, to_emails=to_emails, body=email_body, cc=bcc)
                     email.send()
             else:
                 res = form.save(commit=False)
