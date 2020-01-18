73f01941c28b75fd87dbd43034ae1fee37c1a1af
Ryan LaPointe <ryan@ryanlapointe.org>
2020-01-18 13:17:38 -0500

    Remove dangerous code

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -15,10 +15,10 @@ os.environ['DJANGO_SETTINGS_MODULE'] = "lnldb.settings"
 from django.core.wsgi import get_wsgi_application  # NOQA isort:skip
 application = _application = get_wsgi_application()
 
-# Test application (for when importing doesn't work)
+## Test application (for when importing doesn't work)
 # def application(env, start_response):
 #     start_response('200 OK', [('Content-Type','text/html')])
 #     try:
 #         return _application(env, start_response)
 #     except Exception, e:
-#         return str(os.environ) + str(sys.version) + str(sys.argv) + "\n\n" + str(e)
+#         return str(e)
