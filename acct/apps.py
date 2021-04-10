7832fbf85df7c34e68e4fd887487576dfc650612
Tom Nurse <tjnurse@wpi.edu>
2021-04-09 22:14:38 -0400

    Fix UserSearchAdapter

--- acct/apps.py
+++ acct/apps.py
@@ -7,13 +7,16 @@ class AcctConfig(AppConfig):
     name = 'acct'
     verbose_name = "Account Module"
 
-    # def ready(self):
-    #     User = django.contrib.auth.get_user_model()
-    #
-    #     watson.register(User, UserSearchAdapter, fields=('id', 'email', 'name', 'mdc', 'phone', 'group_str',
-    #                                                      'owns', 'orgs'))
+    def ready(self):
+        User = django.contrib.auth.get_user_model()
+
+        if watson.is_registered(User):
+            watson.unregister(User)
+
+        watson.register(User, UserSearchAdapter, fields=('id', 'email', 'name', 'mdc', 'phone', 'group_str',
+                                                         'owns', 'orgs'))
 
 
 class UserSearchAdapter(watson.SearchAdapter):
     def get_title(self, obj):
-        return obj.profile.fullname
+        return obj.name
