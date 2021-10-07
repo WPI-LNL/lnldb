8bb3aeebd3b784cefdcb59be93698ba4f1fa7d78
Cara Salter <cara@devcara.com>
2021-10-07 10:29:51 -0400

    accounts: Resolve migrations conflict
    A difference between upstream and this branch caused two duplicate
    migration IDs to be leafs of 0004.

--- accounts/migrations/0006_user_pronouns.py
+++ accounts/migrations/0006_user_pronouns.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0004_userpreferences'),
+        ('accounts', '0005_remove_duplicate_perms'),
     ]
 
     operations = [
