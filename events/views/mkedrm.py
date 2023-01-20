efb730f869e455380e3efbaae9596d91eb819054
Benjamin A <yellowjaguar5@gmail.com>
2023-01-20 11:44:55 -0500

    Add tolerance 25Live Reservation code retrieval
    Remove slicing JSON string and add `try/except`

--- events/views/mkedrm.py
+++ events/views/mkedrm.py
@@ -76,12 +76,15 @@ def eventnew(request, id=None):
 
                 # 25Live parsing
                 if is_event2019 and obj.event_id is None:
-                    if form.data.get('reference_code') != "": 
-                        url = requests.get(f"https://25live.collegenet.com/25live/data/wpi/run/list/listdata.json?compsubject=event&obj_cache_accl=0&name={form.data.get('reference_code')}")
-                        text = url.text[6:]
-                        jsondata = json.loads(text)
-                        obj.event_id = jsondata["rows"][0]["contextId"]
-                        obj.save()
+                    if form.data.get('reference_code') != "":
+                        try: 
+                            url = requests.get(f"https://25live.collegenet.com/25live/data/wpi/run/list/listdata.json?compsubject=event&obj_cache_accl=0&name={form.data.get('reference_code')}")
+                            text = url.text
+                            jsondata = json.loads(text)
+                            obj.event_id = jsondata["rows"][0]["contextId"]
+                            obj.save()
+                        except requests.JSONDecodeError:
+                            pass
                     
                 if is_event2019:
                     services_formset.save()
