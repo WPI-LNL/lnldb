26c0b00a36b64f2d3efd7b6c19131463e0cbd641
Benjamin A <yellowjaguar5@gmail.com>
2024-07-21 20:37:03 -0400

    Fix localization of time input fields
    Known bug linked with upcoming deprecation of USE_L10N setting. See here for more info: https://groups.google.com/g/django-developers/c/vBa2-R9BnHY?pli=1

---
+++ lnldb/formats/en/formats.py
@@ -0,0 +1,3 @@
+TIME_INPUT_FORMATS = ['%I:%M %p', '%I:%M:%S.%f %p', '%I:%M %p',
+                      '%I:%M%p', '%I:%M:%S.%f%p', '%I:%M%p',
+                      '%H:%M:%S', '%H:%M:%S.%f', '%H:%M']
