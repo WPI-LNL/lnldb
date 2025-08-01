1ac70725638d7e6c27a4171d6ea5a3c899e29f1a
Ben Feinberg <66845228+benfeinberg@users.noreply.github.com>
2025-08-01 07:57:54 -0400

    rental fixes

--- events/models.py
+++ events/models.py
@@ -860,19 +860,19 @@ class Event2019(BaseEvent):
         return sum(ei.totalcost for ei in self.extrainstance_set.all())
 
     @property
-    def rentals_total(self):
-        return sum(rental.totalcost for rental in self.rentals.all())
-
-    @property
     def rental_fee_total(self):
-        percent = self.pricelist.rental_fee_percentage
+        percent = self.pricelist.rental_fee_percentage if self.pricelist else 15
         applicable_total = sum(decimal.Decimal(rental.totalcost) for rental in self.rentals.filter(rental_fee_applied=True))
         total = applicable_total * decimal.Decimal(percent) / decimal.Decimal("100")
         return total.quantize(decimal.Decimal('.01'), rounding=decimal.ROUND_DOWN)
 
     @property
+    def rentals_total(self):
+        return sum(rental.totalcost for rental in self.rentals.all()) + self.rental_fee_total
+
+    @property
     def cost_total_pre_discount(self):
-        return self.services_total + self.extras_total + self.oneoff_total + self.rentals_total
+        return self.services_total + self.extras_total + self.oneoff_total
 
     @property
     def discount_applied(self):
@@ -930,9 +930,9 @@ class Event2019(BaseEvent):
     @property
     def cost_total(self):
         if self.uses_new_discounts:
-            return self.cost_total_pre_discount - sum(self.discount_values.values()) + sum(self.fee_values.values()) + self.rental_fee_total
+            return self.cost_total_pre_discount - sum(self.discount_values.values()) + sum(self.fee_values.values()) + self.rentals_total
         else:
-            return self.cost_total_pre_discount - self.discount_value + self.rental_fee_total
+            return self.cost_total_pre_discount - self.discount_value
 
     @property
     def workday_form_hash(self):
@@ -1113,7 +1113,7 @@ class Pricelist(models.Model):
     services = models.ManyToManyField(Service, through="ServicePrice", related_name="pricelists")
     fees = models.ManyToManyField(Fee, through="FeePrice", related_name="pricelists")
     discounts = models.ManyToManyField(Discount, through="DiscountPrice", related_name="pricelists")
-    rental_fee_percentage = models.DecimalField(max_digits=8, decimal_places=2)
+    rental_fee_percentage = models.DecimalField(default=15, max_digits=8, decimal_places=2)
 
     def __str__(self):
         return self.name
