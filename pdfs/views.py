895290a92d158f743e664c56d5bf86112fc2eeae
Ben Feinberg <66845228+benfeinberg@users.noreply.github.com>
2026-07-21 12:42:19 -0400

    custon weasyprint local fetcher

--- pdfs/views.py
+++ pdfs/views.py
@@ -17,11 +17,10 @@ from django.utils.text import slugify
 from xhtml2pdf import pisa
 from weasyprint import HTML, CSS
 from weasyprint.urls import URLFetcher, URLFetcherResponse
-from django_weasyprint.utils import DjangoURLFetcher
 try:
     from weasyprint.text.fonts import FontConfiguration
 except ModuleNotFoundError:
-    from weasyprint.text.fonts import FontConfiguration
+    from weasyprint.fonts import FontConfiguration
 from pypdf import PdfWriter
 
 from events.models import Category, BaseEvent, Event2019, ExtraInstance, MultiBilling, Quote
@@ -50,6 +49,13 @@ def link_callback(uri, rel):
         raise Exception('media URI must start with %s or %s' % (surl, murl))
     return path
 
+class LocalFetcher(URLFetcher):
+    def fetch(self, url, headers=None):
+        if url.startswith("file://"):
+            path = link_callback(url[7:], "")
+            return URLFetcherResponse("file://" + path, open(path, 'rb'))
+        else:
+            raise Exception('Non-local files are not implemented for safety reasons')
 
 def generate_pdf(context, template, request):
     # Render html content through html template with context
@@ -237,9 +243,9 @@ def render_quote_to_pdf(quote) -> BytesIO:
             font-style: italic;
         }
         """
-        HTML(string=quote.html, url_fetcher=DjangoURLFetcher(), base_url="file://").write_pdf(
+        HTML(string=quote.html, url_fetcher=LocalFetcher(), base_url="file://").write_pdf(
             pdf_file,
-            stylesheets=[CSS(string=font_string, font_config=font_config, url_fetcher=DjangoURLFetcher())],
+            stylesheets=[CSS(string=font_string, font_config=font_config, url_fetcher=LocalFetcher())],
             font_config=font_config)
     else:
         pisa.CreatePDF(quote.html, dest=pdf_file, link_callback=link_callback)
